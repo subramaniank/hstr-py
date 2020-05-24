6af821217019e963f5c97824c78407a806c372a7
Subramanian Kaleeswaran <subkale@amazon.com>
2020-05-24 14:52:00 -0700

    Revert "minimal cleanup"
    This reverts commit f634d8d34acee1eb881d9349f26a2560bf2bd7f2.

--- hstr.py
+++ hstr.py
@@ -1,19 +1,21 @@
 import sys,os,re,termios,fcntl
 import curses
 import string
-
-from parser.history_parser import HistoryParser
-from processor.input_processor import InputProcessor
+import logging
+current_selected_cmd = ""
 
 CONFIG = {
         'EXTENDED_HISTORY': True,
+        'MAX_MATCHES': 30
 }
+
+logging.basicConfig(filename='/tmp/hstr_subkale.log', level=logging.DEBUG)
 
 def format_curr_search_string(new_char, curr_search_string):
     new_search_string = curr_search_string
     if (chr(new_char) in string.printable):
         new_search_string += chr(new_char)
-    elif new_char == 263 or new_char == 127:
+    elif new_char == 263:
         # backspace
         new_search_string = new_search_string[:-1]
     return new_search_string
@@ -21,26 +23,56 @@ def format_curr_search_string(new_char, curr_search_string):
 def get_cursor_pos(curr_search_stringstr):
     return len(curr_search_stringstr), 0 
 
-def print_formatted_output(output_lines, stdscr, curr_selection, print_offset_from_top):
-    _, width = stdscr.getmaxyx()
-
-    curr_print_index = print_offset_from_top
-    index_to_highlight = curr_selection + print_offset_from_top
-
+def print_formatted_output(output_lines, stdscr, curr_selection):
+    height, width = stdscr.getmaxyx()
+    curr_print_index = 1
     for line in output_lines:
-        if curr_print_index == index_to_highlight:
+        if curr_selection != None and curr_print_index == curr_selection+1:
            stdscr.attron(curses.color_pair(2))
         output_str = line[:width-1].strip() + "\n"
         stdscr.addstr(curr_print_index, 0, output_str)
-        if curr_print_index == index_to_highlight:
+        if curr_selection != None and curr_print_index == curr_selection+1:
            stdscr.attroff(curses.color_pair(2))
         curr_print_index += 1
 
+def get_formatted_cmd(line):
+    if CONFIG['EXTENDED_HISTORY']:
+        return ''.join(line.split(';')[1:])
+    return line
 
+def get_output(curr_search_string, stdscr):
+    f_shell_history = open(os.environ["HISTFILE"])
+    output_lines = set()
+    output_str = ""
+    max_matches = CONFIG['MAX_MATCHES'] 
+    current_match = 0
+    for line in f_shell_history:
+        formatted_cmd = get_formatted_cmd(line)
+        if formatted_cmd and re.search(curr_search_string, formatted_cmd):
+            current_match += 1
+            output_lines.add(formatted_cmd)
+            if current_match >= max_matches:
+                break
+    return list(output_lines)
 
-def draw_menu(stdscr, history_parser, input_processor):
-    curr_selection = 0
-    current_selected_cmd = None
+def get_current_selected_cmd(k, curr_selection, output_lines):
+    if k == curses.KEY_DOWN:
+        if curr_selection == None:
+            curr_selection = -1
+        curr_selection = min(len(output_lines)-1, curr_selection+1)
+        curr_selected_cmd = output_lines[curr_selection]
+    elif k == curses.KEY_UP:
+        if curr_selection == None:
+            curr_selection = 0
+        curr_selection = max(0, curr_selection-1)
+    if curr_selection == None or curr_selection > (len(output_lines) - 1):
+        curr_selected_cmd = None
+    else:
+        curr_selected_cmd = output_lines[curr_selection]
+    return curr_selection, curr_selected_cmd
+
+def draw_menu(stdscr):
+    curr_selection = None
     curr_search_string = ""
     k = 0
     cursor_x = 0
@@ -58,6 +90,7 @@ def draw_menu(stdscr, history_parser, input_processor):
 
     # Loop where k is the last character pressed
     while (True):
+        global current_selected_cmd
 
         # Reset and break on Ctrl-g
         if (k == 7):
@@ -79,32 +112,30 @@ def draw_menu(stdscr, history_parser, input_processor):
         curr_search_stringstr = "Current search string: {}".format(curr_search_string)
         cursor_x, cursor_y = get_cursor_pos(curr_search_stringstr)
 
-        # Parse history and get output
-        output_lines = history_parser.get_output(curr_search_string, height)
-
-        # Get current selected command and index
-        curr_selection, current_selected_cmd = input_processor.get_current_selected_cmd(k, curr_selection, output_lines)
-
-        # Rendering current search at the top
+        # Rendering some text
         stdscr.addstr(0, 0, curr_search_stringstr, curses.color_pair(1))
 
-        # Rendering status bar below current search string
+        output_lines = get_output(curr_search_string, stdscr)
+
+        # Refresh the screen
+        curr_selection, current_selected_cmd = get_current_selected_cmd(k, curr_selection, output_lines)
+        logging.debug(current_selected_cmd)
+        stdscr.move(cursor_y, cursor_x)
+        
         statusbarstr = "Press 'Ctrl-g' to exit | STATUS BAR | Pos: {}, {} | Current Selection {} | {} | Current selected cmd {}".format(cursor_x, cursor_y, curr_selection, keystr, current_selected_cmd)
         # Render status bar
         stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(1, 0, statusbarstr)
+        stdscr.addstr(height-20, 0, statusbarstr)
+        #stdscr.addstr(height-30, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
         stdscr.attroff(curses.color_pair(3))
+        stdscr.refresh()
 
-        print_formatted_output(output_lines, stdscr, curr_selection, 2)
+        print_formatted_output(output_lines, stdscr, curr_selection)
         # Wait for next input
         k = stdscr.getch()
 
-    return current_selected_cmd
-
 def main():
-    history_parser = HistoryParser(os.environ["HISTFILE"], CONFIG['EXTENDED_HISTORY'])
-    input_processor = InputProcessor()
-    current_selected_cmd = curses.wrapper(draw_menu, history_parser, input_processor)
+    curses.wrapper(draw_menu)
     if (current_selected_cmd):
         for curr_char in current_selected_cmd:
             fcntl.ioctl(0, termios.TIOCSTI, curr_char)
